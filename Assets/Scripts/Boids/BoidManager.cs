15c5a72e310ec8627462011db9fd5b08126c6cc4
Jesse <u0803072@utah.edu>
2020-02-28 16:39:18 -0700

    Refactored boids to have spawner and manager in the same class, which appears not to result in any kind of slowdown

--- Assets/Scripts/Boids/BoidManager.cs
+++ Assets/Scripts/Boids/BoidManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BoidManager : MonoBehaviour
 {
+    public enum GizmoType { Never, SelectedOnly, Always }
+
+    public Boid prefab;
+    public float spawnRadius = 10;
+    public int spawnCount = 10;
+    public Color colour;
+    public GizmoType showSpawnRegion;
 
     const int threadGroupSize = 1024;
     public Transform target;
@@ -11,6 +18,7 @@ public class BoidManager : MonoBehaviour
     public BoidSettings settings;
     public ComputeShader compute;
     Boid[] boids;
+
 
     void Start()
     {
@@ -93,5 +101,36 @@ public class BoidManager : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(target.position, settings.distanceEpsilon);
         }
+        if (showSpawnRegion == GizmoType.Always)
+        {
+            DrawGizmos();
+        }
+    }
+    void Awake()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 pos = transform.position + Random.insideUnitSphere * spawnRadius;
+            Boid boid = Instantiate(prefab);
+            boid.transform.position = pos;
+            boid.transform.forward = Random.insideUnitSphere;
+
+            boid.SetColour(colour);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (showSpawnRegion == GizmoType.SelectedOnly)
+        {
+            DrawGizmos();
+        }
+    }
+
+    void DrawGizmos()
+    {
+
+        Gizmos.color = new Color(colour.r, colour.g, colour.b, 0.3f);
+        Gizmos.DrawSphere(transform.position, spawnRadius);
     }
 }
