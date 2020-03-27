e2f3ae5fd1b69af9f02459d4836e692fe6dc7b59
Jesse <u0803072@utah.edu>
2020-03-26 18:08:23 -0600

    Remove redundant Library and Asset folders

--- Assets/Assets/Scripts/GameSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameSettings : MonoBehaviour
-{
-    public bool fullscreen;
-    public int resolution;
-}
--- Assets/Scripts/FadeOut.cs
+++ Assets/Scripts/FadeOut.cs
@@ -8,7 +8,7 @@ public class FadeOut : MonoBehaviour
 {
     public void FadeMe()
     {
-        StartCoroutine (DoFade());
+        StartCoroutine(DoFade());
     }
 
     IEnumerator DoFade()
