31f054352ca629d4cda906815d34a77875232e44
Jesse <u0803072@utah.edu>
2020-04-02 18:41:59 -0600

    Added crashing asteroids

---
+++ Assets/AsteroidCrash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCrash : MonoBehaviour
+{
+    public float rotationStrength = 1000;
+    public float maxMovementForce = 10;
+    public GameObject otherAsteroid;
+    private Rigidbody cacheRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cacheRigidbody = GetComponent<Rigidbody>();
+        Vector3 heading = otherAsteroid.transform.position - transform.position;
+
+        cacheRigidbody.AddTorque(Random.onUnitSphere * rotationStrength * 10000000);
+        cacheRigidbody.AddForce(heading * maxMovementForce * 10000);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
