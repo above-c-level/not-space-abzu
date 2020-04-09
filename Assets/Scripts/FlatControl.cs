f93ac4d854543a45651aac46e077513bb2caee64
Jesse <u0803072@utah.edu>
2020-04-09 17:15:10 -0600

    Fix class name for FlatControl.cs

--- Assets/Scripts/FlatControl.cs
+++ Assets/Scripts/FlatControl.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlatPhysics : MonoBehaviour
+public class FlatControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
