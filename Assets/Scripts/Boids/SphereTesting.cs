a1b76c6e72a6232db98fa77b8b29c00a7b7bf236
Jesse <u0803072@utah.edu>
2020-04-16 22:31:32 -0600

    Fixed project

---
+++ Assets/Scripts/Boids/SphereTesting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereTesting : MonoBehaviour
+{
+    public int numPoints = 300;
+    public float radius = 0.01f;
+    public int highlightedSphere = 0;
+
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Vector3[] directions = new PointsOnSphere(numPoints).directions;
+        for (int i = 0; i < directions.Length; i++)
+        {
+            Vector3 point = directions[i];
+            if (i == highlightedSphere)
+            {
+                Gizmos.DrawSphere(point, radius * 5);
+            }
+            else
+            {
+                Gizmos.DrawSphere(point, radius);
+            }
+        }
+    }
+}
--- Assets/Scripts/TutorialPlayerPlaceholder.cs
+++ Assets/Scripts/TutorialPlayerPlaceholder.cs
@@ -35,6 +35,4 @@ public class TutorialPlayerPlaceholder : MonoBehaviour
     {
         planet = newPlanet;
     }
-
-
 }
